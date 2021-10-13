17397c9bec1a0287bd928c5d87d5120d060a3b09
Ryan <1594741693@qq.com>
2021-10-13 16:19:47 +0800

    添加mysql2插件

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
  * @description 项目的入口文件
  */
 const Koa = require('koa');
+const path = require('path');
+console.log("查看path用法：", path.resolve('wwwroot', '/static_files/png/', '../gif/image.gif'))
 const app = new Koa();
 
 app.use(async (ctx) => {
